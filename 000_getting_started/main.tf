6407b8d402bf9ca9aaa20b0ce6b9cde518de6c20
Zimmer <20681015+zimmerx@users.noreply.github.com>
2024-02-11 22:25:17 -0500

    tf_init_course

---
+++ 000_getting_started/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+
+  #  workspaces {
+  #    name = "getting-started"
+  #  }
+  #}
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "ExamPro"
+
+    workspaces {
+      name = "getting-started"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+locals {
+  project_name = "Andrew"
+}
